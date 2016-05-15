5d066ec437c61aaa03ff97dd4ad2f758bdf4043b
Max Gordon <max@gforge.se>
2016-05-15 22:10:09 +0200

    Split the Docker into smaller components

--- Torch/update.sh
+++ Torch/update.sh
@@ -4,11 +4,21 @@
 export LATEST_TORCH_DISTRO_COMMIT=`git ls-remote -h https://github.com/torch/distro.git master | awk '{ print $1 }'`
 
 # Create Dockerfiles from template
-template="Dockerfile.template"
+template="Dockerfile_base.template"
 shell_format='$BASE:$LATEST_TORCH_DISTRO_COMMIT'
 
-dest="nnx-torch/Dockerfile"
+echo "Create the base-torch Dockerfile"
+dest="base-torch/Dockerfile"
 mkdir -p "$(dirname "$dest")"
 export BASE=nvidia/cuda:7.5-cudnn4-devel
 
 envsubst $shell_format < $template > $dest
+
+echo "Create the nnx-specific Dockerfile"
+dest="nnx-torch/Dockerfile"
+mkdir -p "$(dirname "$dest")"
+
+export BASE=gforge/nnx-base
+template="Dockerfile_nnx.template"
+envsubst $shell_format < $template > $dest
+
