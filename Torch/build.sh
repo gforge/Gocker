d0477e3a9827ac22cd7dc6a1c3c58a721cbc30d5
Max Gordon <max@gforge.se>
2017-01-09 15:05:34 +0100

    Removed sudo from build as the sudo timed out Added prettier notification to build Added argument for choosing build depth

--- Torch/build.sh
+++ Torch/build.sh
@@ -1,21 +1,40 @@
 #!/usr/bin/env bash
 
-echo -e "\e[93m*******************************\e[0m"
-echo -e "\e[93m*\e[0m Building base nvidia docker \e[93m*\e[0m"
-echo -e "\e[93m*******************************\e[0m"
-sudo docker build -t gforge/nvidia-torch-base base-torch/
+set -e
 
-echo -e "\e[93m*******************************\e[0m"
-echo -e "\e[93m*\e[0m Building base torch docker \e[93m*\e[0m"
-echo -e "\e[93m*******************************\e[0m"
-sudo docker build -t gforge/base-torch torch/
+# Run with root privileges, i.e. sudo
+build_depth=${1:-4}
 
-echo -e "\e[93m*******************************\e[0m"
-echo -e "\e[93m*\e[0m Building ext. torch docker \e[93m*\e[0m"
-echo -e "\e[93m*******************************\e[0m"
-sudo docker build -t gforge/extend-base extend-torch/
+echo -e "\e[93m***********************\e[0m"
+echo -e "\e[93m*\e[0m Building at depth ${build_depth} \e[93m*\e[0m"
+echo -e "\e[93m***********************\e[0m"
 
-echo -e "\e[93m*******************************\e[0m"
-echo -e "\e[93m*\e[0m Building nnx torch docker \e[93m*\e[0m"
-echo -e "\e[93m*******************************\e[0m"
-sudo docker build -t gforge/nnx-torch nnx-torch/
+if [ $build_depth -ge 4 ]; then
+  echo -e "\e[93m*******************************\e[0m"
+  echo -e "\e[93m*\e[0m Building base nvidia docker \e[93m*\e[0m"
+  echo -e "\e[93m*******************************\e[0m"
+  docker build -t gforge/nvidia-torch-base base-torch/
+fi
+
+if [ $build_depth -ge 3 ]; then
+  echo -e "\e[93m*******************************\e[0m"
+  echo -e "\e[93m*\e[0m Building base torch docker \e[93m*\e[0m"
+  echo -e "\e[93m*******************************\e[0m"
+  docker build -t gforge/base-torch torch/
+fi
+
+if [ $build_depth -ge 2 ]; then
+  echo -e "\e[93m*******************************\e[0m"
+  echo -e "\e[93m*\e[0m Building ext. torch docker \e[93m*\e[0m"
+  echo -e "\e[93m*******************************\e[0m"
+  docker build -t gforge/extend-toch extend-torch/
+fi
+
+if [ $build_depth -ge 1 ]; then
+  echo -e "\e[93m*******************************\e[0m"
+  echo -e "\e[93m*\e[0m Building nnx torch docker \e[93m*\e[0m"
+  echo -e "\e[93m*******************************\e[0m"
+  docker build -t gforge/nnx-torch nnx-torch/
+fi
+
+
