4d995ec5eb69b6c80363ba13a040af5842bd3466
Max Gordon <max@gforge.se>
2017-11-30 22:14:07 +0100

    Spelling error

--- R/R-dev/resources/check_pkg.sh
+++ R/R-dev/resources/check_pkg.sh
@@ -6,14 +6,14 @@ if [ ! -f $DESC_FILE ]; then
   echo "Make sure you mounted the package using --volume /pkd/dir:/root/pkg"
   exit 1
 fi
- 
+
 PKG_NAME=$(sed -n -e '1p' -e '/^Package:/!d' $DESC_FILE \
   | awk '{gsub("Package:[ ]*","")}1')
-  
+
 echo "Building package: $PKG_NAME"
 RD CMD build /root/pkg
 
 PKG_BUILD_NAME=$(find /root/ -name $PKG_NAME*.tar.gz)
-echo "Testing the '$PKG_BUILD_NAME' pacakge"
+echo "Testing the '$PKG_BUILD_NAME' package"
 RD CMD check --as-cran $PKG_BUILD_NAME
 
