08b4a0f99f2ce6bfa40337125626fd9ca7e7c0f0
Max Gordon <max@gforge.se>
2018-02-07 08:48:31 +0100

    Removed old container stuff

---
+++ PyTorch/build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Run with root privileges, i.e. sudo
+build_depth=${1:-2}
+
+echo -e "\e[93m***********************\e[0m"
+echo -e "\e[93m*\e[0m Building at depth ${build_depth} \e[93m*\e[0m"
+echo -e "\e[93m***********************\e[0m"
+
+if [ $build_depth -ge 2 ]; then
+  echo -e "\e[93m*******************************\e[0m"
+  echo -e "\e[93m*\e[0m Building base nvidia docker \e[93m*\e[0m"
+  echo -e "\e[93m*******************************\e[0m"
+  nvidia-docker build -t gforge/base-pytorch base-pytorch/
+fi
+
+if [ $build_depth -ge 1 ]; then
+  echo -e "\e[93m*******************************\e[0m"
+  echo -e "\e[93m*\e[0m Building nnx pytorch docker \e[93m*\e[0m"
+  echo -e "\e[93m*******************************\e[0m"
+  nvidia-docker build -t gforge/nnx-pytorch nnx-pytorch/
+fi
---
+++ PyTorch/update.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+################
+# First Docker #
+################
+
+# Create Dockerfiles from template
+template="Dockerfile_base.template"
+shell_format='$BASE'
+
+echo "Create the base-pytorch Dockerfile"
+dest="base-pytorch/Dockerfile"
+mkdir -p "$(dirname "$dest")"
+export BASE=nvidia/cuda:9.1-cudnn7-devel
+
+envsubst $shell_format < $template > $dest
+
+###############
+# Next Docker #
+###############
+echo "Create the nnx-specific Dockerfile"
+dest="nnx-pytorch/Dockerfile"
+mkdir -p "$(dirname "$dest")"
+
+export BASE=gforge/nvidia-pytorch
+template="Dockerfile_nnx.template"
+envsubst $shell_format < $template > $dest
