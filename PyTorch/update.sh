5ac5067c90be38027577c955bf3d8f58f5ac721f
Max Gordon <max@gforge.se>
2018-06-23 01:18:11 +0200

    Added ability to specify pytorch version

--- PyTorch/update.sh
+++ PyTorch/update.sh
@@ -6,12 +6,18 @@
 
 # Create Dockerfiles from template
 template="Dockerfile_base.template"
-shell_format='$BASE'
+shell_format='${BASE},${PYVER}'
 
 echo "Create the base-pytorch Dockerfile"
 dest="base-pytorch/Dockerfile"
 mkdir -p "$(dirname "$dest")"
 export BASE=nvidia/cuda:9.1-cudnn7-devel
+if [ $# -eq 0 ]
+then
+    export PYVER=''
+else
+    export PYVER="=$1"
+fi
 
 envsubst $shell_format < $template > $dest
 
@@ -21,6 +27,14 @@ envsubst $shell_format < $template > $dest
 echo "Create the nnx-specific Dockerfile"
 dest="nnx-pytorch/Dockerfile"
 mkdir -p "$(dirname "$dest")"
+
+if [ $# -gt 0 ]
+then
+    # Use the docker with the specific PyTorch version
+    # - note - this has to be manually built and sent
+    #          to the docker hub
+    export PYVER=":pytorch$1"
+fi
 
 export BASE=gforge/base-pytorch
 template="Dockerfile_nnx.template"
