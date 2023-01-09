22d8a61809853a50b9a500ef0b4b85216e3f8e1d
Max Gordon <max@gforge.se>
2023-01-09 08:29:16 +0100

    Updated cuda check to work with pytorch-cuda

--- PyTorch/base-pytorch/resources/check_pytorch_cuda.sh
+++ PyTorch/base-pytorch/resources/check_pytorch_cuda.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 
-pytorch_cuda=$(conda list | grep "^pytorch " | grep cuda | wc -l)
+pytorch_cuda=$(conda list | grep "pytorch-cuda" | wc -l)
 
 if [[ "$pytorch_cuda" -eq "0" ]]; then
     echo "Failed to install PyTorch with cuda support";
